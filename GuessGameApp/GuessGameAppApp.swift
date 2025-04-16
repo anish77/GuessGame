93d1dbe2c7b111e9c97fa1cf3f0673bd39f3855f
anacvasniuc <annycvasniuc@yahoo.com>
2025-04-16 07:43:42 +0200

    Initial Commit

---
+++ GuessGameApp/GuessGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameAppApp.swift
+//  GuessGameApp
+//
+//  Created by Ana Cvasniuc on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
