dd0bad6c90d698f0241d6067e8e3a2d9f4cb5cc5
anacvasniuc <annycvasniuc@yahoo.com>
2025-04-16 08:16:41 +0200

    using SwiftUI

--- GuessGameApp/ContentView.swift
+++ GuessGameApp/ContentView.swift
@@ -10,14 +10,42 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                Text("Score 0")
+                
+            }
+            Button(action: {
+                print("test")
+            }, label: {
+                Image("cat")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 24.0))
+                
+            })
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image("fox")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 24.0))
+            })
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image("shiba")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 24.0))
+            })
+            
+            
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
